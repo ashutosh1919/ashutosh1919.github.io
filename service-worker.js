d9e424f1e4fa0fbbf284dc34c0236136730abf5c
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-03-22 11:43:33 +0530

    PRs updated

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f332f5cf52023d9dbdfc6d02da63c6d.js"
+  "/precache-manifest.0b9d829e4a2bacbe897916c44410ca30.js"
 );
 
 self.addEventListener('message', (event) => {
